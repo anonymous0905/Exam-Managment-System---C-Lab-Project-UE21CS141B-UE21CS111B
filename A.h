5540abae0bc8d5b9a03373ab3fb506277095881a
anonymous0905 <pes2202100762@pesu.pes.edu>
2022-06-03 09:40:58 +0530

    read

--- A.h
+++
@@ -1,9 +0,0 @@
-//
-// Created by shash on 13-05-2022.
-//
-
-#ifndef EXAM_MANAGMENT_SYSTEM___C_LAB_PROJECT_A_H
-#define EXAM_MANAGMENT_SYSTEM___C_LAB_PROJECT_A_H
-
-#endif //EXAM_MANAGMENT_SYSTEM___C_LAB_PROJECT_A_H
-int func (int n);
--- main.c
+++ main.c
@@ -1,6 +1,47 @@
-#include <stdio.h>
-#include "A.h"
-int main()
+// C program to Open a File,
+// Read from it, And Close the File
+
+# include <stdio.h>
+# include <string.h>
+
+int main( )
 {
-    func(5);
+
+    // Declare the file pointer
+    FILE *filePointer ;
+
+    // Declare the variable for the data to be read from file
+    char dataToBeRead[50];
+
+    // Open the existing file GfgTest.c using fopen()
+    // in read mode using "r" attribute
+    filePointer = fopen("C:\\Users\\shash\\CLionProjects\\Exam-Managment-System---C-Lab-Project\\hello.txt", "r") ;
+
+    // Check if this filePointer is null
+    // which maybe if the file does not exist
+    if ( filePointer == NULL )
+    {
+        printf( "GfgTest.c file failed to open." ) ;
+    }
+    else
+    {
+
+        printf("The file is now opened.\n") ;
+
+        // Read the dataToBeRead from the file
+        // using fgets() method
+        while( fgets ( dataToBeRead, 50, filePointer ) != NULL )
+        {
+
+            // Print the dataToBeRead
+            printf( "%s" , dataToBeRead ) ;
+        }
+
+        // Closing the file using fclose()
+        fclose(filePointer) ;
+
+        printf("Data successfully read from file GfgTest.c\n");
+        printf("The file is now closed.") ;
+    }
+    return 0;
 }
